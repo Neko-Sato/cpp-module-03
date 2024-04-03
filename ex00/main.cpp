7fb263303c1cc126cb178f31da8a4aee1c87d47b
Neko-Sato <hisatosi35350@gmail.com>
2024-04-04 06:29:01 +0900

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap clapTrap("Clappy");
+	clapTrap.attack("Enemy");
+	clapTrap.takeDamage(10);
+	clapTrap.beRepaired(5);
+	clapTrap.attack("Enemy");
+	clapTrap.takeDamage(20);
+	clapTrap.beRepaired(10);
+	return 0;
+}
